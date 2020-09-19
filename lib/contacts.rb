391c9b38af9616718b5e89b0b99cf91bef50ae4f
J K <kj688895@gmail.com>
2020-09-19 16:37:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,6 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
@@ -25,4 +24,4 @@ def remove_strawberry(contacts)
       end
     end
   end
-end 
+end
